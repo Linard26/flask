dc6d7f3fe1baddfdfd64b8c35985519201cf4482
Maks <20052601l@gmail.com>
2024-04-22 01:18:03 +0300

    модель для базы данных

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r' %self.id
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +29,7 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
