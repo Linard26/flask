b2c06b345ee6bddcfafbbb21423553d8f5226ac3
Maks <20052601l@gmail.com>
2024-04-21 19:29:05 +0300

    html-шаблоны

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    return 'About us'
+    return render_template('about.html')
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
-    return 'User page'+ name + '-'+ id
+    return 'User page: '+ name + '-'+ str(id)
 
 
 if __name__ == '__main__':
