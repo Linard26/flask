ec71e0d12d5e77800befcdeef4da2e64fd0bd79a
Maks <20052601l@gmail.com>
2024-04-22 18:47:56 +0300

    Завершение сайта

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
@@ -33,9 +34,47 @@ def about():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.data).all()
-
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template('posts_detail.html', article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'Произошла ошибка'
+
+
+app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method== 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+
+    else:
+        article = Article.query.get(id)
+        return render_template('post_update.html', article=article)
+
+
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
